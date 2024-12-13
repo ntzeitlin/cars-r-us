b21aaee38e3aa18568f44b5656303815c25fc9c9
Nate Z <ntzeitlin@gmail.com>
2024-12-13 09:23:36 -0600

    scaffold index.html

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const render = () => {
+
+    const composedHTML = `
+        <h1>Cars R' Us: Personal Car Builder</h1>
+
+        <section class="choices">
+
+            <article class="options">
+                <h2>Paints</h2>
+            </article>
+
+            <article class="options">
+                <h2>Interior</h2>
+            </article>
+
+            <article class="options">
+                <h2>Wheels</h2>
+            </article>
+
+            <article class="options">
+                <h2>Technologies</h2>
+            </article>
+
+        </section>
+
+        <section class="order">
+            <p>button goes here</p>
+        </section>
+            
+        <section class="custom-order-list">
+            <p>order list goes here</p>    
+        </section>
+    `
+    document.querySelector("#container").innerHTML = composedHTML
+
+}
+
+render()
